d880e00b455b25152d5056a9c03e85809c49f36a
Pete Shaw <peeta.shaw@gmail.com>
2016-03-09 08:31:26 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		app: ['./lib/index.js']
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'cycle-socket.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
+};
